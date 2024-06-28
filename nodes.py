f4f3710ae1d4c1f0b3440330bad2336b58a203ad
GreenLandisaLie <80653235+GreenLandisaLie@users.noreply.github.com>
2024-06-28 06:45:01 +0100

    Add files via upload

--- nodes.py
+++ nodes.py
@@ -1,44 +1,66 @@
-import os.path
+import os
+from pathlib import Path
 import json
-from PIL import Image
-import comfy.utils
 import folder_paths
-from aura_sr import AuraSR
+from comfy import model_management
+import comfy.utils
+from .aura_sr import AuraSR
+from .utils import *
 
+
+folder_paths.folder_names_and_paths["aura-sr"] = ([os.path.join(folder_paths.models_dir, "aura-sr")], folder_paths.supported_pt_extensions)
 
 class AuraSRUpscaler:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"model_name": (folder_paths.get_filename_list("aura-sr"), ),
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {"required": {"model_name": (folder_paths.get_filename_list("aura-sr"),),
                              "image": ("IMAGE",),
+                             "reapply_transparency": ("BOOLEAN", {"default": True}),
                              "tile_batch_size": ("INT", {"default": 8, "min": 1, "max": 32}),
-                             }}
+                             "device": (["default", "cpu"],),
+                             "offload_to_cpu": ("BOOLEAN", {"default": False}),
+            },
+            "optional": {
+                "transparency_mask": ("MASK",),
+            },
+        }
     
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "main"
 
-    #CATEGORY = "loaders"
+    CATEGORY = "AuraSR"
     
     def __init__(self):
         self.loaded = False
         self.model_name = ""
         self.aura_sr = None
+        self.upscaling_factor = 4
+        self.device_warned = False
     
     
-    def unload(): # I don't know if this is the best way to unload a model but it should work
-        self.aura_sr.upsampler = None
+    def unload(self):
+        if self.aura_sr is not None:
+            self.aura_sr.upsampler = None  # I don't know if this is the best way to unload a model but it should work
         self.aura_sr = None
         self.loaded = False
         self.model_name = ""
+        self.upscaling_factor = 4
     
     
-    def load(model_name):
+    def load(self, model_name, device):
         model_path = folder_paths.get_full_path("aura-sr", model_name)
         config_path = model_path[:model_path.rindex('.')] + ".json"
         config_path = config_path if os.path.isfile(config_path) else model_path.replace(model_name, "config.json")
         
-        device = model_management.get_torch_device()
-        config = json.loads(config_path.read_text())
+        config = json.loads(Path(config_path).read_text())
+        
+        try:
+            self.upscaling_factor = int(config["image_size"] / config["input_image_size"])
+        except:
+            print(f"Failed to calculate {model_name}'s upscaling factor. Defaulting to 4.")
+            self.upscaling_factor = 4
         
         checkpoint = comfy.utils.load_torch_file(model_path, safe_load=True)
         
@@ -49,17 +71,40 @@ class AuraSRUpscaler:
         self.model_name = model_name
     
     
-    def main(self, model_name, image, tile_batch_size):
+    def main(self, model_name, image, reapply_transparency, tile_batch_size, device, offload_to_cpu, transparency_mask=None):
+        torch_device = model_management.get_torch_device()
+        if model_management.directml_enabled and device == "default":
+            device = "cpu"
+            if not self.device_warned:
+                print("Cannot run AuraSR on DirectML device. Using CPU instead (this will be VERY SLOW!)")
+                self.device_warned = True
+        else:
+            device = torch_device
+        
         if not self.loaded or self.model_name != model_name:
             self.unload()
-            self.load(model_name)
+            self.load(model_name, device)
         else:
-            device = model_management.get_torch_device()
             self.aura_sr.upsampler.to(device)
-            
-        upscaled_image = self.aura_sr.upscale_4x(image)
         
-        self.aura_sr.upsampler.to("cpu")
+        image, resized_alpha = prepare_input(image, transparency_mask, reapply_transparency, self.upscaling_factor)        
+        
+        try:
+            upscaled_image = self.aura_sr.upscale_4x(image=image, max_batch_size=tile_batch_size)
+        except:
+            print("Failed to upscale with AuraSR. Returning original image.")
+            upscaled_image = image
+        
+        if reapply_transparency and resized_alpha is not None:
+            try:
+                upscaled_image = paste_alpha(upscaled_image, resized_alpha)
+            except:
+                print("Failed to apply alpha layer.")
+        
+        upscaled_image = pil2tensor(upscaled_image)
+        
+        if offload_to_cpu:
+            self.aura_sr.upsampler.to("cpu")
         
         return (upscaled_image, )
 
---
+++ utils.py
@@ -0,0 +1,90 @@
+import torch
+import numpy as np
+from PIL import Image
+
+
+def pil2tensor(image):
+    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
+
+def tensor2pil(image):
+    return Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(0), 0, 255).astype(np.uint8))
+
+def numpy2pil(image):
+    return Image.fromarray(np.clip(255. * image.squeeze(0), 0, 255).astype(np.uint8))
+
+def to_pil(image):
+    if isinstance(image, Image.Image):
+        return image
+    if isinstance(image, torch.Tensor):
+        return tensor2pil(image)
+    if isinstance(image, np.ndarray):
+        return numpy2pil(image)
+    raise ValueError(f"Cannot convert {type(image)} to PIL.Image")
+
+def has_transparency(image):
+    if isinstance(image, Image.Image):
+        if image.info.get("transparency", None) is not None:
+            return True
+        if image.mode == "P":
+            transparent = image.info.get("transparency", -1)
+            for _, index in image.getcolors():
+                if index == transparent:
+                    return True
+        elif image.mode == "RGBA":
+            extrema = image.getextrema()
+            if extrema[3][0] < 255:
+                return True
+    
+    if isinstance(image, torch.Tensor) or isinstance(image, np.ndarray):
+        return True if image.shape[-1] == 4 else False
+        
+    return False
+
+def get_resized_alpha(image, transparency_mask, upscaling_factor):
+    try:
+        if transparency_mask is not None:
+            if image.shape[:3] != transparency_mask.shape[:3] and len(transparency_mask.shape) != len(image.shape) + 1:
+                # Invalid mask. Attempt with original image
+                if has_transparency(image):
+                    img = image
+                else:
+                    return None
+            else:
+                img = transparency_mask
+                img = 1.0 - img # invert
+                img = img.reshape((-1, 1, img.shape[-2], img.shape[-1])).movedim(1, -1).expand(-1, -1, -1, 3) # mask -> image
+        else:
+            if has_transparency(image):
+                img = image
+            else:
+                return None
+        
+        if isinstance(img, torch.Tensor):
+            img = img.clone().cpu().numpy()
+        if isinstance(img, np.ndarray):
+            mode = 'RGBA' if transparency_mask is None else 'RGB'
+            img = Image.fromarray(np.clip(255. * img.squeeze(0), 0, 255).astype(np.uint8), mode=mode)
+            if not img.getbbox(): # some RGB images return fully black masks with the 'Load Image' node - cannot apply masking if so
+                return None
+        
+        resized_alpha = img.resize((img.width * upscaling_factor, img.height * upscaling_factor)).split()[-1]
+    except:
+        return None
+    return resized_alpha
+
+def paste_alpha(image, alpha):
+    image = image.convert("RGBA")
+    image.putalpha(alpha)
+    return image
+
+
+def prepare_input(image, transparency_mask, reapply_transparency, upscaling_factor):
+    resized_alpha = get_resized_alpha(image, transparency_mask, upscaling_factor) if reapply_transparency else None
+    image = to_pil(image)
+    if resized_alpha is not None:
+        image = image.convert("RGB")
+    return image, resized_alpha
+
+
+
+ 
